b0a425a9f713954c631dc55a38b33f0494fa9a32
rylee-frank <rfmackenzie25@gmail.com>
2021-03-07 15:04:17 -0700

    functioning images

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@
 
 function Image(title, url, author, color, tags) {
 	this.title = title;
-	this.url = url;
+	this.url = "url(imgs/" + url;
 	this.author = author;
 	this.color = color;
 	this.tags = tags;
@@ -21,7 +21,7 @@ function Image(title, url, author, color, tags) {
 		var imageString = "";
 		imageString += "<div>";
 		//imageString += "<div style='background-color:" + this.color + "'>"
-		imageString += "<div style='background-image: url('imgs/" + this.url + "')'>";
+		imageString += "<div style='background-image:" + this.url + ")'>";
 		imageString += "</div>";
 		imageString += "<h1>" + this.title + "</h1>";
 		imageString += "<h2>" + this.author + "</h2>";
@@ -35,9 +35,9 @@ var img2 = new Image("Frozen in Time", "frozen1.jpg", "Rylee Frank", "#90dfde", 
 var img3 = new Image("Grow Tall", "growth1.jpg", "Rylee Frank", "#90dfb5", ["nature", "plant", "tree"]);
 var img4 = new Image("Stand Strong", "canyon-21.jpg", "Rylee Frank", "#df9090", ["nature", "tree", "canyon", "winter", "view"]);
 var img5 = new Image("Cut Your Own Path", "canyon1.jpg", "Rylee Frank", "#dfd190", ["nature", "canyon", "winter"]);
-var img6 = new Image("Look Ahead", "mountain-view1.jpg", "Rylee Frank", "#b3df90", ["nature", "view", "mountain"]);
-var img7 = new Image("Clarity", "reflection11.jpeg", "Rylee Frank", "#90dfad", ["nature", "water", "mountain"]);
-var img8 = new Image();
+var img6 = new Image("Look Ahead", "mountain-view11.jpg", "Rylee Frank", "#b3df90", ["nature", "view", "mountain"]);
+var img7 = new Image("Clarity", "reflection1.jpg", "Rylee Frank", "#90dfad", ["nature", "water", "mountain"]);
+var img8 = new Image("Just Keep Moving", "snowy-stream1.jpg", "Rylee Frank", "#90cddf", ["nature", "winter", "water"]);
 var img9 = new Image();
 var img10 = new Image();
 
@@ -48,3 +48,4 @@ img4.display();
 img5.display();
 img6.display();
 img7.display();
+img8.display();

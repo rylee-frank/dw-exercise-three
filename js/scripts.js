f65b5e93a93545aed1ccf28f8327576ad25663f6
rylee-frank <rfmackenzie25@gmail.com>
2021-03-08 12:20:50 -0700

    functioning buttons, no images

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 
-function Image(title, url, author, color, tags) {
-	this.title = title;
-	this.url = "url(imgs/" + url;
-	//this.url = "url(../imgs/" + url;
+function Image (title, url, author, tags) {
+	this. title = title;
+	this.url = "imgs/" + url;
 	this.author = author;
 	this.tags = tags;
+
 	this.display = function() {
-		var imageString = "";
-		imageString += "<div class = 'unit' id = '" + tags + "'>";
-		imageString += "<div class='border' style='background-image:" + this.url + ")'>";
+		var container = $("<div>")
+		this.tags.forEach(function(tag){
+			container.addClass(tag);
+	})
+	container.addClass("image")
+	var imageString = ""
+		imageString += "<div>";
+		imageString += "<div style='background-image:" + this.url + ")'>";
+ 		imageString += "</div>";
 		imageString += "</div>";
 		imageString += "<div class = 'info'>";
 		imageString += "<h1>" + this.title + "</h1>";
@@ -16,60 +22,49 @@ function Image(title, url, author, color, tags) {
 		imageString += "</div>";
 		imageString += "</div>";
 
-		$("main").prepend(imageString);
-
-		// var container = $("<div>")
-		// this.tags.forEach(function(tag){
-		// 	container.addClass(tag);
-		// 	$(".buttons").prepend("<button>" + tag + "</button>")
-		// })
-
-		// container.css("background-image", this.url)
-
-		// var imageString = "";
-		// imageString += "<div class = 'info'>";
-		// imageString += "<h1>" + this.title + "</h1>";
-		// imageString += "<h2>" + this.author + "</h2>";
-		// imageString += "</div>";
-
-		// container.html(imageString)
-		// $("main").prepend(container);
+		container.html(imageString)
+		$(".images").prepend(container)
 	}
 }
-var images = [
-	new Image("Peace in a Moment", "wheat1.jpg", "Rylee Frank", ["nature", "winter", "plant"]),
-	new Image("Frozen in Time", "frozen1.jpg", "Rylee Frank", ["nature", "winter", "waterfall"]),
-	new Image("Grow Tall", "growth1.jpg", "Rylee Frank", ["nature", "plant", "tree"]),
-	new Image("Stand Strong", "canyon-21.jpg", "Rylee Frank", ["nature", "tree", "canyon", "winter", "view"]),
-	new Image("Cut Your Own Path", "canyon1.jpg", "Rylee Frank", ["nature", "canyon", "winter"]),
-	new Image("Look Ahead", "mountain-view11.jpg", "Rylee Frank", ["nature", "view", "mountain"]),
-	new Image("Clarity", "reflection1.jpg", "Rylee Frank", ["nature", "water", "mountain"]),
-	new Image("Just Keep Moving", "snowy-stream1.jpg", "Rylee Frank", ["nature", "winter", "water"]),
-	new Image("Push Forward", "uphill1.jpg", "Rylee Frank", ["nature", "mountain", "plant"]),
-	new Image("Prepare to Fall", "waterfall1.jpg", "Rylee Frank", ["nature", "waterfall", "water", "mountain"]),
-	new Image("Go With the Flow", "stream1.jpg", "Rylee Frank", ["nature", "waterfall", "water"]),
-	new Image("Go Where You Must", "waterfall2.jpg", "Rylee Frank", ["nature", "waterfall", "water", "tree"])
-]
 
-images.forEach(function(image){
- 	image.display();
- }) 
+var Images = [
+new Image("Peace in a Moment", "wheat.jpg", "Rylee Frank", ["nature", "winter", "plant"]),
+new Image("Frozen in Time", "frozen1.jpg", "Rylee Frank", ["nature", "winter", "waterfall"]),
+new Image("Grow Tall", "growth1.jpg", "Rylee Frank", ["nature", "plant", "tree"]),
+new Image("Stand Strong", "canyon-21.jpg", "Rylee Frank", ["nature", "tree", "canyon", "winter", "view"]),
+new Image("Cut Your Own Path", "canyon1.jpg", "Rylee Frank", ["nature", "canyon", "winter"],
+new Image("Look Ahead", "mountain-view11.jpg", "Rylee Frank", ["nature", "view", "mountain"]),
+new Image("Clarity", "reflection1.jpg", "Rylee Frank", ["nature", "water", "mountain"]),
+new Image("Just Keep Moving", "snowy-stream1.jpg", "Rylee Frank", ["nature", "winter", "water"]),
+new Image("Push Forward", "uphill1.jpg", "Rylee Frank", ["nature", "mountain", "plant"]),
+new Image("Prepare to Fall", "waterfall1.jpg", "Rylee Frank", ["nature", "waterfall", "water", "mountain"]),
+new Image("Go With the Flow", "stream1.jpg", "Rylee Frank", ["nature", "waterfall", "water"]),
+new Image("Go Where You Must", "waterfall2.jpg", "Rylee Frank", ["nature", "waterfall", "water", "tree"])
 
-var tag = ["nature", "winter", "plant", "waterfall", "tree", "canyon", "view", "mountain", "water"]
-var tagList = "";
-tag.forEach(function(tags) {
-	tagList += "<li class = 'filter' id = '" +tags+ "''>" + "<h4 class = 'button'>" + tags + "</h4>" + "</li>";
-	
-});
-
-$(".buttons").html(tagList);
+)]
+var tagList = []
+Images.forEach(function(image){
+	image.display();
+	image.tags.forEach(function(tag) {
+		if (!tagList.includes(tag)) {
+			tagList.push(tag);
+			$(".buttons").prepend("<button class='filter' id = '" + tag + "'>" + tag + "</button>")
+		}
+	})
+})
 
 $(".filter").on("click", function() {
-	var id = $(this).attr("id");
-	console.log(id)
-	$("." + id).fadeIn();
-	$(".unit").not("." + id).hide();
-})  
+	var tag = $(this).attr("id");
+	console.log(tag)
+
+	$("." + tag).fadeIn();
+	$(".image").not("." + tag).hide();
+
+	$(".filter").removeClass("active")
+	$(this).addClass("active")
+})
+
+
 
 
 
